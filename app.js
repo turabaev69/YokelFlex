374b31d6d20d009ecc052a00a4dc7b9f0d064706
odom50 <kbibi0865@gmail.com>
2022-11-08 01:30:39 +0900

    updated

--- app.js
+++ app.js
@@ -14,31 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    const firstName = req.body.fName;
-    // const lastName = req.body.lName;
-    // const email = req.body.email;
-
-    // const data = {
-    //     members: [{
-    //         email_address: email,
-    //         status: "subscribed",
-    //         merge_fields: {
-    //             FNAME: firstName,
-    //             LNAME: lastName
-    //         }
-    //     }]
-    // };
-
-    // const jsonData = JSON.stringify(data);
-
-    // const listId = process.env.LIST_ID;
-    // const serverNum = process.env.SERVER_NUM;
-    // const url = `https://us${serverNum}.api.mailchimp.com/3.0/lists/${listId}`;
-
-    // const options = {
-    //     method: "POST",
-    //     auth: `jojoliu:${process.env.MAIL_API_KEY}`
-    // };
+    // const firstName = req.body.fName;
 
     const request = https.request(url, options, (response) => {
 
